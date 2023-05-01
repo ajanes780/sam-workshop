865f76e96316be8a663636a75a85254e1faf7b81
Aaron Janes <aaron.janes@gmail.com>
2023-04-30 20:10:31 -0600

    lab 4 completed aws cognito and userpools

--- ORDER_APP/src/order-api/functions/post-orders/app.js
+++ ORDER_APP/src/order-api/functions/post-orders/app.js
@@ -30,7 +30,7 @@ exports.postOrders = async (event) => {
     let parsedBody = JSON.parse(body);
 
     let item = {
-      user_id : "static_user",
+    user_id: parsedBody.cognito_userid,
       id: messageId,
       name: parsedBody.data.name,
       restaurantId: parsedBody.data.restaurantId,
